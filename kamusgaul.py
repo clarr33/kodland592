7ce2297dc4079bcfaf55c4fb81c31c449e98eb76
clarr33 <145128678+clarr33@users.noreply.github.com>
2023-09-15 20:46:35 +0700

    Create kamusgaul.py

---
+++ kamusgaul.py
@@ -0,0 +1,17 @@
+# Kamus Bahasa Gaul
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "CREEPY": "Menakutkan, tidak menyenangkan",
+            "ROFL": "Tanggapan terhadap lelucon",
+            "SHEESH": "Sedikit ketidaksetujuan",
+            "AGGRO": "Untuk menjadi agresif/marah"
+            }
+            
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("Maaf, kata tidak ditemukan!")
